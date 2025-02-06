c0a136a069ade520c328ce0b2b244543a733f8b9
vivekrawat1998 <vr90888561@gmail.com>
2025-02-06 13:03:47 +0530

    vercel.json is set for deployment

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -1,10 +1,15 @@
-
 import React, { createContext, useEffect } from 'react';
 import { io } from 'socket.io-client';
 
 export const SocketContext = createContext();
 
-const socket = io(`${import.meta.env.VITE_BASE_URL}`); 
+const socket = io(`${import.meta.env.VITE_BASE_URL}`, {
+    transports: ['websocket'],
+    withCredentials: true,
+    extraHeaders: {
+        "Access-Control-Allow-Origin": "https://uberclonefrontend.vercel.app"
+    }
+}); 
 
 const SocketProvider = ({ children }) => {
     useEffect(() => {
@@ -12,13 +17,20 @@ const SocketProvider = ({ children }) => {
             console.log('Connected to server');
         });
 
+        socket.on('connect_error', (error) => {
+            console.error('Connection error:', error);
+        });
+
         socket.on('disconnect', () => {
             console.log('Disconnected from server');
         });
 
+        return () => {
+            socket.off('connect');
+            socket.off('connect_error');
+            socket.off('disconnect');
+        };
     }, []);
-
-
 
     return (
         <SocketContext.Provider value={{ socket }}>
